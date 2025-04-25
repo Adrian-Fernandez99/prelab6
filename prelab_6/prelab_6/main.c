8a9e2e1d669dd49141ee1f690cf4cf1bbd07582c
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-04-25 10:19:20 -0600

    Creación
    Se crea en base al postlab pasado|

---
+++ prelab_6/prelab_6/main.c
@@ -0,0 +1,18 @@
+/*
+ * prelab_6.c
+ *
+ * Created: 4/25/2025 10:11:37 AM
+ * Author : adria
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
